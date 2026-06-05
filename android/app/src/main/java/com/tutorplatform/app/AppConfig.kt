c3223917d1dbabbb80b8b1cee67ab5f2c566b6df
vit <vit@orbisystems.ru>
2026-06-05 09:56:27 +0300

    r

--- android/app/src/main/java/com/tutorplatform/app/AppConfig.kt
+++ android/app/src/main/java/com/tutorplatform/app/AppConfig.kt
@@ -2,7 +2,7 @@ package com.tutorplatform.app
 
 object AppConfig {
     const val API_BASE_URL = "http://192.168.1.167/"
-    const val KEYCLOAK_BASE_URL = "http://192.168.1.167/auth/"
+    const val KEYCLOAK_BASE_URL = "$API_BASE_URL/auth/"
     const val KEYCLOAK_REALM = "tutorapp"
     const val KEYCLOAK_CLIENT_ID = "tutorapp-client"
 }
